683c85fb16064122cac2385a037bd374b618532e
hellojibon4 <hellojibon4@gmail.com>
2025-09-28 20:41:54 -0700

    button and style shete add

--- src/Compunent/Countries/countries.jsx
+++ src/Compunent/Countries/countries.jsx
@@ -1,18 +1,21 @@
 import React, { use } from 'react';
 import Country from '../Country/Country';
+import './Countries.css'
 
 const Countries = ({ countriesPromise }) => {
     const countriesData = use(countriesPromise);
     const countries = countriesData.countries
-    
+
     return (
-        <div>
+        <div >
             <h1>In the countries: {countries.length} </h1>
-            {
-                countries.map(country => <Country
-                    key={country.ccn3.ccn3}
-                    country={country }> </Country>)
-            }
+            <div className='Countries'>
+                {
+                    countries.map(country => <Country
+                        key={country.ccn3.ccn3}
+                        country={country}> </Country>)
+                }
+            </div>
         </div>
     );
 };
--- src/Compunent/Country/Country.jsx
+++ src/Compunent/Country/Country.jsx
@@ -1,16 +1,23 @@
 import React from 'react';
+import './Country.css'
 
 const Country = ({ country }) => {
-    console.log(country)
+    // console.log(country)
+    const handalVisited =() =>{
+        console.log("button Clicked")
+    }
     return (
-        <div>
+        <div className='country'>
             <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
             <h3>Name: {country.name.common} </h3>
             <h3>CCN: {country.ccn3.ccn3} </h3>
-            <h3>population: {country.population.population} </h3>
-            <h3>Area: {country.area.area} </h3>
             <h3>Region: {country.region.region} </h3>
+            <h3>Area: {country.area.area} {
+                country.area.area > 30000 ? "Big Countru" : "Small Country"
+            } </h3>
+            <h3>population: {country.population.population} </h3>
             <h3></h3>
+            <button onClick={handalVisited}>Not Visited</button>
         </div>
     );
 };
